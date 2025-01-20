bd0aee9a9dfc6d4fda56b194f79976755c79d5a3
BrendoFDev <brendo.dkry@gmail.com>
2025-01-20 17:58:35 -0300

    criar fonte para o envio de mensagens

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function() { 
-    const bttjoinRoom = $('#joinRoom');
-    
-    const socket = io("http://localhost:3000", {
-        reconnectionDelayMax: 10000
+    const bttjoinRoom = $('#bttJoinRoom');
+    const bttSendMessage = $('#bttSendMessage');
+    const userName = $('#userName');
+    const messageInput = $('#messageInput');
+    const messageBox = $('#box_messages');
+    const roomName = $('#roomName');
+
+    const socket = io("http://192.168.0.75:3000", {
+        transports: ["websocket"],
+        upgrade: false
     });
     
     bttjoinRoom.click((event) => {
@@ -10,8 +16,7 @@ $(document).ready(function() {
     });
     
     function joinRoom() {
-        const roomName = $('#roomName');
-        console.log(roomName.val())
+        
         if (roomName.val()) {  
             socket.emit("join_private_room", { roomId: roomName.val() });
             console.log("tentando entrar na sala: ", roomName.val());
@@ -20,7 +25,41 @@ $(document).ready(function() {
         }
     }
     
-    socket.on('joined_in_room', (data) => {
-        alert(data.response);
+    bttSendMessage.click((event)=>{
+        event.preventDefault();
+        sendMessage();
     });
+
+    function sendMessage(){
+
+        if(messageInput.val() && userName.val())
+            socket.emit('private_message', {
+        roomId: roomName.val(),
+        message: messageInput.val(),
+        sender: userName.val()
+    });
+
+        messageInput.val('');
+
+    }
+
+    socket.on('receive_message',(data)=>{
+        showMessageInScreen(data.sender, data.message)
+    });
+
+    function showMessageInScreen(userName,message){
+        messageBox.append(
+            `
+            <div class="message">
+            <div class="sender">
+                <label>${userName}</label>
+            </div>
+            <div class="message_data">
+                ${message}
+            </div>
+            </div>
+            `
+        );
+        console.log('plotado')
+    }
 });
